4664ff7c86c33c44794570d3732e8cd57a668790
Isaac Saraiva <isaacsaraiva.vinke@outlook.com>
2023-02-22 16:58:26 -0300

    1. Added Lazy.Vinke.Json to batch builders

--- 1.0.0/Builder.Debug.bat
+++ 1.0.0/Builder.Debug.bat
@@ -3,3 +3,7 @@
 cd .\Modules\Lazy.Vinke\Sources\
 call .\Lazy.Vinke.Builder.Debug.bat
 cd ..\..\..\
+
+cd .\Modules\Lazy.Vinke.Json\Sources\
+call .\Lazy.Vinke.Json.Builder.Debug.bat
+cd ..\..\..\
--- 1.0.0/Builder.Release.bat
+++ 1.0.0/Builder.Release.bat
@@ -3,3 +3,7 @@
 cd .\Modules\Lazy.Vinke\Sources\
 call .\Lazy.Vinke.Builder.Release.bat
 cd ..\..\..\
+
+cd .\Modules\Lazy.Vinke.Json\Sources\
+call .\Lazy.Vinke.Json.Builder.Release.bat
+cd ..\..\..\
