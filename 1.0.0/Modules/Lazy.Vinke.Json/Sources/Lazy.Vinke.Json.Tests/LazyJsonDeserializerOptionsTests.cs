5394288f9c366320a66eeea3c63d7d3f16599fac
Isaac Saraiva <isaacsaraiva.vinke@outlook.com>
2023-02-23 16:21:53 -0300

    1. Added new test cases

---
+++ 1.0.0/Modules/Lazy.Vinke.Json/Sources/Lazy.Vinke.Json.Tests/LazyJsonDeserializerOptionsTests.cs
@@ -0,0 +1,34 @@
+// LazyJsonDeserializerOptionsTests.cs
+//
+// This file is integrated part of Lazy project
+// Licensed under "Gnu General Public License Version 3"
+//
+// Created by Isaac Bezerra Saraiva
+// Created on 2023, February 23
+
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Lazy.Vinke.Json;
+
+namespace Lazy.Vinke.Json.Tests
+{
+    [TestClass]
+    public class LazyJsonDeserializerOptionsTests
+    {
+        [TestMethod]
+        public void TestDeserializerOptions()
+        {
+            // Arrange
+            LazyJsonDeserializerOptionsBase deserializerOptionsBase = null;
+            LazyJsonDeserializerOptions deserializerOptions = new LazyJsonDeserializerOptions();
+
+            // Act
+            deserializerOptionsBase = deserializerOptions.Item<LazyJsonDeserializerOptionsBase>();
+
+            // Assert
+            Assert.IsNotNull(deserializerOptionsBase);
+            Assert.IsTrue(deserializerOptions.Contains<LazyJsonDeserializerOptionsBase>());
+        }
+    }
+}
