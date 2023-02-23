f41aa324dc5a59fd97ecf7a3530abf1f1f91bfa2
Isaac Saraiva <isaacsaraiva.vinke@outlook.com>
2023-02-23 14:29:30 -0300

    1. Improved test cases

--- 1.0.0/Modules/Lazy.Vinke.Json/Sources/Lazy.Vinke.Json.Tests/LazyJsonDeserializerDataTableTests.cs
+++ 1.0.0/Modules/Lazy.Vinke.Json/Sources/Lazy.Vinke.Json.Tests/LazyJsonDeserializerDataTableTests.cs
@@ -98,18 +98,26 @@ namespace Lazy.Vinke.Json.Tests
         {
             // Arrange
             LazyJsonObject jsonObjectCurrent = new LazyJsonObject();
-            jsonObjectCurrent.Add("Integer1", new LazyJsonInteger(-50));
-            jsonObjectCurrent.Add("Integer2", new LazyJsonInteger(0));
-            jsonObjectCurrent.Add("Integer3", new LazyJsonInteger(2075));
+            jsonObjectCurrent.Add("DBNull", new LazyJsonNull());
+
+            jsonObjectCurrent.Add("Boolean1", new LazyJsonBoolean(true));
+            jsonObjectCurrent.Add("Boolean2", new LazyJsonBoolean(false));
+            jsonObjectCurrent.Add("BooleanDBNull", new LazyJsonBoolean(null));
+
             jsonObjectCurrent.Add("String1", new LazyJsonString("Json"));
             jsonObjectCurrent.Add("String2", new LazyJsonString("DataTable"));
             jsonObjectCurrent.Add("String3", new LazyJsonString("Tests"));
-            jsonObjectCurrent.Add("Boolean1", new LazyJsonBoolean(true));
-            jsonObjectCurrent.Add("Boolean2", new LazyJsonBoolean(false));
+            jsonObjectCurrent.Add("StringDBNull", new LazyJsonString(null));
+
+            jsonObjectCurrent.Add("Integer1", new LazyJsonInteger(-50));
+            jsonObjectCurrent.Add("Integer2", new LazyJsonInteger(0));
+            jsonObjectCurrent.Add("Integer3", new LazyJsonInteger(2075));
+            jsonObjectCurrent.Add("IntegerDBNull", new LazyJsonInteger(null));
+
             jsonObjectCurrent.Add("Decimal1", new LazyJsonDecimal(1.2m));
             jsonObjectCurrent.Add("Decimal2", new LazyJsonDecimal(34.56m));
             jsonObjectCurrent.Add("Decimal3", new LazyJsonDecimal(789.123m));
-            jsonObjectCurrent.Add("Null1", new LazyJsonNull());
+            jsonObjectCurrent.Add("DecimalDBNull", new LazyJsonDecimal(null));
 
             LazyJsonObject jsonObjectValues = new LazyJsonObject();
             jsonObjectValues.Add("Original", new LazyJsonNull());
@@ -136,18 +144,27 @@ namespace Lazy.Vinke.Json.Tests
             Assert.IsTrue(((DataTable)resDataTable).TableName == "WithRows");
             Assert.IsTrue(((DataTable)resDataTable).Rows.Count == 1);
             Assert.IsTrue(((DataTable)resDataTable).Rows[0].RowState == DataRowState.Unchanged);
-            Assert.IsTrue((Int32)((DataTable)resDataTable).Rows[0]["Integer1"] == -50);
-            Assert.IsTrue((Int32)((DataTable)resDataTable).Rows[0]["Integer2"] == 0);
-            Assert.IsTrue((Int32)((DataTable)resDataTable).Rows[0]["Integer3"] == 2075);
+
+            Assert.IsTrue(((DataTable)resDataTable).Rows[0]["DBNull"] == DBNull.Value);
+
+            Assert.IsTrue((Boolean)((DataTable)resDataTable).Rows[0]["Boolean1"] == true);
+            Assert.IsTrue((Boolean)((DataTable)resDataTable).Rows[0]["Boolean2"] == false);
+            Assert.IsTrue(((DataTable)resDataTable).Rows[0]["BooleanDBNull"] == DBNull.Value);
+
             Assert.IsTrue((String)((DataTable)resDataTable).Rows[0]["String1"] == "Json");
             Assert.IsTrue((String)((DataTable)resDataTable).Rows[0]["String2"] == "DataTable");
             Assert.IsTrue((String)((DataTable)resDataTable).Rows[0]["String3"] == "Tests");
-            Assert.IsTrue((Boolean)((DataTable)resDataTable).Rows[0]["Boolean1"] == true);
-            Assert.IsTrue((Boolean)((DataTable)resDataTable).Rows[0]["Boolean2"] == false);
+            Assert.IsTrue(((DataTable)resDataTable).Rows[0]["StringDBNull"] == DBNull.Value);
+
+            Assert.IsTrue((Int32)((DataTable)resDataTable).Rows[0]["Integer1"] == -50);
+            Assert.IsTrue((Int32)((DataTable)resDataTable).Rows[0]["Integer2"] == 0);
+            Assert.IsTrue((Int32)((DataTable)resDataTable).Rows[0]["Integer3"] == 2075);
+            Assert.IsTrue(((DataTable)resDataTable).Rows[0]["IntegerDBNull"] == DBNull.Value);
+
             Assert.IsTrue((Decimal)((DataTable)resDataTable).Rows[0]["Decimal1"] == 1.2m);
             Assert.IsTrue((Decimal)((DataTable)resDataTable).Rows[0]["Decimal2"] == 34.56m);
             Assert.IsTrue((Decimal)((DataTable)resDataTable).Rows[0]["Decimal3"] == 789.123m);
-            Assert.IsTrue(((DataTable)resDataTable).Rows[0]["Null1"] == DBNull.Value);
+            Assert.IsTrue(((DataTable)resDataTable).Rows[0]["DecimalDBNull"] == DBNull.Value);
         }
     }
 }
