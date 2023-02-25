89166caeef47414ef73679bfc04c728f0b65d320
Isaac Saraiva <isaacsaraiva.vinke@outlook.com>
2023-02-24 23:21:31 -0300

    1. Added new test cases

---
+++ 1.0.0/Modules/Lazy.Vinke.Json/Sources/Lazy.Vinke.Json.Tests/Serialization/LazyJsonSerializer/LazyJsonSerializerDateTimeTests.cs
@@ -0,0 +1,102 @@
+// LazyJsonSerializerDateTimeTests.cs
+//
+// This file is integrated part of Lazy project
+// Licensed under "Gnu General Public License Version 3"
+//
+// Created by Isaac Bezerra Saraiva
+// Created on 2023, February 24
+
+using System;
+
+using Lazy.Vinke.Json;
+
+namespace Lazy.Vinke.Json.Tests
+{
+    [TestClass]
+    public class LazyJsonSerializerDateTimeTests
+    {
+        [TestMethod]
+        public void TestSerializerDateTimeDataNull()
+        {
+            // Arrange
+            LazyJsonToken resToken = null;
+            Object data = null;
+            LazyJsonSerializerDateTime serializerDateTime = new LazyJsonSerializerDateTime();
+
+            // Act
+            resToken = serializerDateTime.Serialize(data);
+
+            // Assert
+            Assert.IsTrue(resToken.Type == LazyJsonType.Null);
+        }
+
+        [TestMethod]
+        public void TestSerializerDateTimeDataNotDateTime()
+        {
+            // Arrange
+            LazyJsonToken resToken = null;
+            Object data = 10;
+            LazyJsonSerializerDateTime serializerDateTime = new LazyJsonSerializerDateTime();
+
+            // Act
+            resToken = serializerDateTime.Serialize(data);
+
+            // Assert
+            Assert.IsTrue(resToken.Type == LazyJsonType.Null);
+        }
+
+        [TestMethod]
+        public void TestSerializerDateTimeWithoutSerializationOptions()
+        {
+            // Arrange
+            LazyJsonToken resToken = null;
+            DateTime data = DateTime.Now;
+            LazyJsonSerializerDateTime serializerDateTime = new LazyJsonSerializerDateTime();
+
+            // Act
+            resToken = serializerDateTime.Serialize(data);
+
+            // Assert
+            Assert.IsTrue(resToken.Type == LazyJsonType.String);
+            Assert.IsTrue(((LazyJsonString)resToken).Value == data.ToString());
+        }
+
+        [TestMethod]
+        public void TestSerializerDateTimeWithSerializationOptions()
+        {
+            // Arrange
+            LazyJsonToken resToken = null;
+            DateTime data = DateTime.Now;
+            LazyJsonSerializerDateTime serializerDateTime = new LazyJsonSerializerDateTime();
+
+            LazyJsonSerializerOptions serializerOptions = new LazyJsonSerializerOptions();
+            serializerOptions.Item<LazyJsonSerializerOptionsDateTime>().Format = TestStringFormat.DateTime.ISO8601Z;
+
+            // Act
+            resToken = serializerDateTime.Serialize(data, serializerOptions);
+
+            // Assert
+            Assert.IsTrue(resToken.Type == LazyJsonType.String);
+            Assert.IsTrue(((LazyJsonString)resToken).Value == data.ToString(TestStringFormat.DateTime.ISO8601Z));
+        }
+
+        [TestMethod]
+        public void TestSerializerDateTimeNullableWithSerializationOptions()
+        {
+            // Arrange
+            LazyJsonToken resToken = null;
+            DateTime? data = DateTime.Now;
+            LazyJsonSerializerDateTime serializerDateTime = new LazyJsonSerializerDateTime();
+
+            LazyJsonSerializerOptions serializerOptions = new LazyJsonSerializerOptions();
+            serializerOptions.Item<LazyJsonSerializerOptionsDateTime>().Format = TestStringFormat.DateTime.ISO8601Z;
+
+            // Act
+            resToken = serializerDateTime.Serialize(data, serializerOptions);
+
+            // Assert
+            Assert.IsTrue(resToken.Type == LazyJsonType.String);
+            Assert.IsTrue(((LazyJsonString)resToken).Value == ((DateTime)data).ToString(TestStringFormat.DateTime.ISO8601Z));
+        }
+    }
+}
