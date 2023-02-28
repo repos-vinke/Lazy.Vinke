1812649db1daef9c1a914f7b150f64216b84b208
Isaac Saraiva <isaacsaraiva.vinke@outlook.com>
2023-02-28 07:52:06 -0300

    1. Added new test cases

--- 1.0.0/Modules/Lazy.Vinke.Database/Sources/Lazy.Vinke.Database.Tests/Globals.cs
+++ 1.0.0/Modules/Lazy.Vinke.Database/Sources/Lazy.Vinke.Database.Tests/Globals.cs
@@ -11,4 +11,9 @@ public static class Globals
     {
         public static string ConnectionString = "Server=localhost;Port=5432;Database=lazy;User Id=lazy;Password=lazy;";
     }
+
+    public static class SqlServer
+    {
+        public static string ConnectionString = "Server=.\\SQLExpress;Database=lazy;User Id=lazy;Password=lazy;Encrypt=no";
+    }
 }
---
+++ 1.0.0/Modules/Lazy.Vinke.Database/Sources/Lazy.Vinke.Database.Tests/TestLazyDatabase/TestLazyDatabaseSqlServer.cs
@@ -0,0 +1,78 @@
+﻿// TestLazyDatabaseSqlServer.cs
+//
+// This file is integrated part of Lazy project
+// Licensed under "Gnu General Public License Version 3"
+//
+// Created by Isaac Bezerra Saraiva
+// Created on 2023, February 27
+
+using System;
+using System.Text;
+
+using Lazy.Vinke;
+using Lazy.Vinke.Database;
+using Lazy.Vinke.Database.SqlServer;
+
+namespace Lazy.Vinke.Database.Tests
+{
+    [TestClass]
+    public class TestLazyDatabaseSqlServer : TestLazyDatabase
+    {
+        [TestMethod]
+        public override void TestQueryExecute()
+        {
+            // Arrange
+            String sqlCreate = @"create table QueryExecuteTest (Id integer, Name varchar(32))";
+            String sqlInsert = @"insert into QueryExecuteTest (Id, Name) values (1, 'Lazy'),(2, 'Vinke')";
+            String sqlUpdate = @"update QueryExecuteTest set Name = 'Database' where Id = :Id";
+            String sqlDelete = @"delete from QueryExecuteTest where Id = :Id";
+            String sqlDrop = @"drop table if exists QueryExecuteTest";
+
+            // Act
+            LazyDatabase database = new LazyDatabaseSqlServer(Globals.SqlServer.ConnectionString);
+
+            database.OpenConnection();
+            database.QueryExecute(sqlCreate, null);
+
+            Int32 rowsInserted = database.QueryExecute(sqlInsert, null);
+            Int32 rowsUpdated = database.QueryExecute(sqlUpdate, new Object[] { 2 });
+            Int32 rowsDeleted = database.QueryExecute(sqlDelete, new Object[] { 1 });
+
+            database.QueryExecute(sqlDrop, null);
+            database.CloseConnection();
+
+            // Assert
+            Assert.IsTrue(rowsInserted == 2);
+            Assert.IsTrue(rowsUpdated == 1);
+            Assert.IsTrue(rowsDeleted == 1);
+        }
+
+        [TestMethod]
+        public override void TestQueryValue()
+        {
+            // Arrange
+            String sqlCreate = @"create table TestQueryValue (Id integer, Name varchar(32))";
+            String sqlInsert = @"insert into TestQueryValue (Id, Name) values (1, 'Lazy'),(2, 'Vinke')";
+            String sqlSelect = @"select Name from TestQueryValue where Id = :Id";
+            String sqlDrop = @"drop table if exists TestQueryValue";
+
+            // Act
+            LazyDatabase database = new LazyDatabaseSqlServer(Globals.SqlServer.ConnectionString);
+
+            database.OpenConnection();
+            database.QueryExecute(sqlCreate, null);
+
+            Int32 rowsInserted = database.QueryExecute(sqlInsert, null);
+            Object value1 = database.QueryValue(sqlSelect, new Object[] { 1 });
+            Object value2 = database.QueryValue(sqlSelect, new Object[] { 2 });
+
+            database.QueryExecute(sqlDrop, null);
+            database.CloseConnection();
+
+            // Assert
+            Assert.IsTrue(rowsInserted == 2);
+            Assert.IsTrue((String)value1 == "Lazy");
+            Assert.IsTrue((String)value2 == "Vinke");
+        }
+    }
+}
